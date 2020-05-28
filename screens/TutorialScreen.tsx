90016bc89d9948a9f0da88591b998b12cdaeac29
peterzernia <peter.zernia@medigo.com>
2020-05-28 22:05:02 +0200

    intro screen styling

--- screens/TutorialScreen.tsx
+++ screens/TutorialScreen.tsx
@@ -1,57 +1,42 @@
 import React from 'react'
 import {
-  View, StyleSheet, AsyncStorage, Dimensions,
+  View, StyleSheet, AsyncStorage, Text as RNText, Platform,
 } from 'react-native'
 import Text from 'components/Text'
 import AppIntroSlider from 'react-native-app-intro-slider'
+import { MaterialIcons } from '@expo/vector-icons'
 import colors from 'utils/colors'
 
 const slides = [
   {
     key: '1',
-    title: "Welcome to Let's Fork",
-    text: 'swipe right on your next favorite restaurant',
-    backgroundColor: colors.purple,
+    text: 'Swipe right on your\nnext favorite restaurant',
   },
   {
     key: '2',
-    title: "Let's Fork",
-    text: 'makes deciding on food with friends easy',
-    backgroundColor: colors.blue,
+    text: "Deciding on food doesn't have to be hard",
   },
   {
     key: '3',
-    title: 'Get Started',
-    text: 'by creating or joining a party and\nswiping with your friends',
-    backgroundColor: colors.persianGreen,
+    text: 'Get started by creating a party,\nchoosing your location, and inviting\nfriends to start swiping',
   },
 ]
 
 type Slide = {
   key: string;
-  title: string;
   text: string;
-  backgroundColor: string;
 }
 
 type Props = {
   setShowApp: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const { height } = Dimensions.get('screen')
-
 const TutorialScreen = React.memo((props: Props): React.ReactElement => {
   const { setShowApp } = props
 
 
   const renderItem = ({ item }: { item: Slide }): React.ReactElement => (
-    <View
-      style={{
-        ...styles.slide,
-        backgroundColor: item.backgroundColor,
-      }}
-    >
-      <Text style={styles.title}>{item.title}</Text>
+    <View style={styles.slide}>
       <Text style={styles.text}>{item.text}</Text>
     </View>
   )
@@ -63,32 +48,52 @@ const TutorialScreen = React.memo((props: Props): React.ReactElement => {
   }
 
   return (
-    <AppIntroSlider
-      renderItem={renderItem}
-      data={slides}
-      onDone={onDone}
-    />
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <MaterialIcons style={styles.icon} name="restaurant" color={colors.white} size={26} />
+        <RNText style={styles.header}>Let&apos;s Fork</RNText>
+      </View>
+      <AppIntroSlider
+        renderItem={renderItem}
+        data={slides}
+        onDone={onDone}
+      />
+    </View>
   )
 })
 
 export default TutorialScreen
 
 const styles = StyleSheet.create({
-  slide: {
-    height,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingBottom: 120,
+  container: {
+    backgroundColor: colors.purple,
+    flex: 1,
+    paddingTop: 40,
   },
-  title: {
+  headerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontFamily: 'VarelaRound_400Regular',
     color: colors.white,
     fontSize: 32,
     textAlign: 'center',
-    paddingTop: 20,
-    paddingBottom: 20,
+    paddingTop: Platform.OS === 'ios' ? 3 : 0,
+  },
+  icon: {
+    paddingRight: 8,
+  },
+  slide: {
+    flexGrow: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 40,
   },
   text: {
+    fontSize: 16,
     color: colors.white,
     textAlign: 'center',
   },
